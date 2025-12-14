5c97577958f5a0bb431279656c7daf73685f9e56
SAYED ADINAN <147618795+sayedadinan@users.noreply.github.com>
2025-12-14 16:09:16 +0530

    Trigger redeploy for new customer APIs

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const app = require('./src/app');
 const connectDB = require('./src/config/database');
 
 const PORT = process.env.PORT || 5000;
+// Deployment timestamp: 2025-12-14
 
 // Connect to MongoDB
 connectDB();
